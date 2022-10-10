e5b88997d1f6082ead8170d691370a2a04f4e959
William Saulon <willfrombuffalo13@gmail.com>
2022-10-10 15:22:47 -0400

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Home from "./pages/Home";
+import Toys from "./pages/Toys";
+import About from "./pages/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/toys" element={<Toys />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
