3a18a0ad2aa0b19c837f66138ebc5410dbed228b
William Saulon <willfrombuffalo13@gmail.com>
2022-10-10 16:18:43 -0400

    Hit backend for GET request

--- src/pages/Toys.js
+++ src/pages/Toys.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import "../styles/toys.css"
 
 function Toys() {
+  const [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/toys', configObj)
+      .then(res => res.json())
+      .then(data => setToys(data))
+  }, [])
+
+  useEffect(() => {
+    console.log(toys)
+  }, [toys])
+
+ const configObj = {
+  method: 'GET',
+  headers: {'Content-Type': 'application/json'}
+ }
+ 
   return (
-    <div>Toys</div>
+    <div>
+      {toys.map((toy) => (
+        <div className="toy-container">
+          <p>ID: {toy.id}</p>
+          <p>Name: {toy.name}</p>
+          <img className="toys-img" src={toy.image} />
+          <p>Likes: {toy.likes}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
