441cb70808fa12841ef01570f395feddaf866122
William Saulon <willfrombuffalo13@gmail.com>
2022-11-12 21:47:17 -0500

    Added Project Goals/Requirements to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,30 @@ import "../styles/about.css";
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return ( 
+    <div className ="About">
+      <h1>About</h1>
+      <h2>Project Goals</h2>
+        <ul>
+          <li>1. To build a React single-page application (Containing one index.html file) </li>
+          <li>2. Test knowledge of components, props, and state management</li>
+          <li>3. Implement client-side routing</li>
+          <li>4. Use data from an API</li>
+        </ul>
+      <h2>Project Requirements</h2>
+        <ul>
+          <li>2. The application must have more than or equal to 5 components</li>
+          <li>3. The application must have </li>
+        </ul>
+    </div>
+     );
+
+     
 }
+
+
+
+
+
 
 export default About;
