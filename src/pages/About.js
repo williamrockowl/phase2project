a54c01df2ff05bc9318dfe3f0feb08a8e7639809
William Saulon <willfrombuffalo13@gmail.com>
2022-11-12 22:02:32 -0500

    Added Content to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,19 +4,24 @@ import React from "react";
 function About() {
   return ( 
     <div className ="About">
-      <h1>About</h1>
+      
       <h2>Project Goals</h2>
+        <div id = "list">
         <ul>
-          <li>1. To build a React single-page application (Containing one index.html file) </li>
+          <li>1. To build a React single-page application (from scratch) </li>
           <li>2. Test knowledge of components, props, and state management</li>
           <li>3. Implement client-side routing</li>
           <li>4. Use data from an API</li>
         </ul>
       <h2>Project Requirements</h2>
         <ul>
+          <li>1. Make a single page application (using one index.html file)</li>
           <li>2. The application must have more than or equal to 5 components</li>
-          <li>3. The application must have </li>
+          <li>3. The application must have at least 3 client-side routes</li>
+          <li>4. Use a json-server to create an API for your backend</li>
+          <li>5. Have some styling for the application</li>
         </ul>
+        </div>
     </div>
      );
 
