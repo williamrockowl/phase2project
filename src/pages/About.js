2966f5fc49bd147caee70711ba141338fe65f908
William Saulon <willfrombuffalo13@gmail.com>
2022-11-12 21:17:12 -0500

    Changed Title and Linked CSS to About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,4 @@
+import "../styles/about.css";
 import React from "react";
 
 function About() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <h1>Toy Story Database</h1>;
+  return <h1>Drag N Drop Toys</h1>;
 }
 
 export default Home;
